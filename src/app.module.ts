d36a8ffbe9f00a11017e44392fd0d8f767fcc867
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-21 15:42:58 +0530

    feat(app): register RedisModule and FeedModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RedisModule } from './redis/redis.module';
+import { FeedModule } from './feed/feed.module';
 
 @Module({
-  imports: [],
+  imports: [RedisModule, FeedModule],
   controllers: [AppController],
   providers: [AppService],
 })
