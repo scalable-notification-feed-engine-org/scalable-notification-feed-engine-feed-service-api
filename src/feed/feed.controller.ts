6685e37b125dfa79c128af9a9a4d8cbca149d516
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-21 15:44:00 +0530

    feat(feed): add REST and Kafka handlers for getFeed, post.created, and post.liked

---
+++ src/feed/feed.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import * as createPost from '../interfaces/create.post';
+import { FeedService } from './feed.service';
+import * as updatePost from '../interfaces/update.like';
+
+@Controller()
+export class FeedController {
+  constructor(
+    private readonly feedService: FeedService,
+  ) {}
+
+  @Get(':userId')
+  async getFeed(@Param('userId') userId: string) {
+    const createPostDtos = await this.feedService.getFeedForUser(userId);
+    console.log('createPostDtos', createPostDtos);
+    return createPostDtos;
+  }
+
+  @MessagePattern('post.created')
+  async handlePostCreated(@Payload() data: createPost.CreatePostDto) {
+    console.log('New post received from Kafka:', data);
+
+    await this.feedService.processAndStorePost(data);
+
+    return { status: 'success' };
+  }
+
+  @MessagePattern('post.liked')
+  async handlePostLiked(@Payload() data: updatePost.UpdateLike) {
+    console.log('New post received from Kafka:', data);
+
+    await this.feedService.updateLikeCount(data);
+
+    return { status: 'success' };
+  }
+}
