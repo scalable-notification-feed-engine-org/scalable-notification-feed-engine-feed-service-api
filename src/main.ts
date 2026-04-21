22df49c0d3f74146a12317d5c05d0ba858794045
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-21 15:45:22 +0530

    feat(server): add Kafka microservice, enable CORS, and set port to 3003

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'feed-service-consumer',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3003);
 }
 bootstrap();
